0105886300c1a4e56f9ba5b5530524945bc16d07
FelipeGK <felipegrondek00@gmail.com>
2023-04-11 14:33:00 -0300

    feat: add announcements mock and images

---
+++ client/src/mocks/announcements/index.ts
@@ -0,0 +1,484 @@
+export const announcements = [
+    {
+        "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58292",
+        "model": "C4 CACTUS Rip Curl 1.6 16V Flex Aut.",
+        "brand": "Citroën",
+        "year": 2022,
+        "fuel": 1,
+        "km": 76323,
+        "color": "Cinza",
+        "fip_price": 112077,
+        "price": 102123,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel urna et nisi luctus auctor eget vitae magna. Sed viverra ullamcorper nulla eu efficitur.",
+        "cover_image": "cars_images/CITROEN_C4_CACTUS_1.6.webp",
+        "gallery_images": [
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/Citroen-C4-Cactus-Rip-Curl-2.jpg"
+            },
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/CITROEN_C4_CACTUS_1.6.webp"
+            }
+        ],
+        "is_active": true,
+        "created_at": "2023-04-11T07:56:23.430Z",
+        "updated_at": "2023-04-11T07:56:23.430Z",
+        "owner": {
+            "id": "ff46341a-62b2-47c8-8d1f-fc87d7ec3f17",
+            "name": "Lucas Magalhães"
+        }
+      },
+      {
+        "id": "83c0316f-f66a-4138-8163-e6f891d90db2",
+        "model": "C3 Attraction 1.6 Flex 16V 5p Aut.",
+        "brand": "Citroën",
+        "year": 2022,
+        "fuel": 1,
+        "km": 15756,
+        "color": "Verde",
+        "fip_price": 66957,
+        "price": 61574,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel urna et nisi luctus auctor eget vitae magna. Sed viverra ullamcorper nulla eu efficitur.",
+        "cover_image": "cars_images/C3_Attraction_2022_1.webp",
+        "gallery_images": [
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/C3_Attraction_2022_1.webp"
+            },
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/C3_Attraction_2022_2.png"
+            }
+        ],
+        "is_active": true,
+        "created_at": "2023-04-11T07:56:23.430Z",
+        "updated_at": "2023-04-11T07:56:23.430Z",
+        "owner": {
+            "id": "ff46341a-62b2-47c8-8d1f-fc87d7ec3f17",
+            "name": "Lucas Magalhães"
+        }
+      },
+      {
+        "id": "ab70c8db-0b15-42a1-b47d-cf85a2e2e1f7",
+        "model": "C4 LOUNGE Live 1.6 Turbo Flex Aut.",
+        "brand": "Citroën",
+        "year": 2022,
+        "fuel": 1,
+        "km": 59649,
+        "color": "Branco",
+        "fip_price": 73549,
+        "price": 68690,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel urna et nisi luctus auctor eget vitae magna. Sed viverra ullamcorper nulla eu efficitur.",
+        "cover_image": "cars_images/C4_LOUNGE_Live_1.6_1.webp",
+        "gallery_images": [
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/C4_LOUNGE_Live_1.6_2.jpg"
+            },
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/C4_LOUNGE_Live_1.6_1.webp"
+            }
+        ],
+        "is_active": true,
+        "created_at": "2023-04-11T07:56:23.430Z",
+        "updated_at": "2023-04-11T07:56:23.430Z",
+        "owner": {
+            "id": "ff46341a-62b2-47c8-8d1f-fc87d7ec3f17",
+            "name": "Lucas Magalhães"
+        }
+      },
+      {
+        "id": "dcc0b858-ba6f-4f90-a8e7-4dd0f0cb7c61",
+        "model": "Strada Adventure1.8/ 1.8 LOCKER Flex CD",
+        "brand": "Fiat",
+        "year": 2020,
+        "fuel": 1,
+        "km": 0,
+        "color": "Preto",
+        "fip_price": 91600,
+        "price": 99168,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel urna et nisi luctus auctor eget vitae magna. Sed viverra ullamcorper nulla eu efficitur.",
+        "cover_image": "cars_images/fiat_strada_2020_1.jpg",
+        "gallery_images": [
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/fiat_strada_2020_1.jpg"
+            },
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/fiat_strada_2020_2.jpg"
+            }
+        ],
+        "is_active": true,
+        "created_at": "2023-04-11T10:20:30.000Z",
+        "updated_at": "2023-04-11T10:20:30.000Z",
+        "owner": {
+            "id": "ff46341a-62b2-47c8-8d1f-fc87d7ec5g44",
+            "name": "Gabriela Marchiori"
+        }
+      },
+      {
+        "id": "9acfffc9-31bb-4e99-b74a-81f7c31e06d2",
+        "model": "ARGO HGT 1.8 16V Flex Mec.",
+        "brand": "Fiat",
+        "year": 2019,
+        "fuel": 1,
+        "km": 0,
+        "color": "Azul",
+        "fip_price": 72140,
+        "price": 78125,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel urna et nisi luctus auctor eget vitae magna. Sed viverra ullamcorper nulla eu efficitur.",
+        "cover_image": "cars_images/argo_hgt_2019_1.jpg",
+        "gallery_images": [
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/argo_hgt_2019_2.jpg"
+            },
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/argo_hgt_2019_1.jpg"
+            }
+        ],
+        "is_active": true,
+        "created_at": "2023-04-11T10:20:30.000Z",
+        "updated_at": "2023-04-11T10:20:30.000Z",
+        "owner": {
+            "id": "ff46341a-62b2-47c8-8d1f-fc87d7ec5g44",
+            "name": "Gabriela Marchiori"
+        }
+      },
+      {
+        "id": "f79c92a6-414e-4d85-9c68-3f9b2a77e14e",
+        "model": "ARGO PRECISION 1.8 16V Flex Mec.",
+        "brand": "Fiat",
+        "year": 2019,
+        "fuel": 1,
+        "km": 0,
+        "color": "Branco",
+        "fip_price": 69036,
+        "price": 72000,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel urna et nisi luctus auctor eget vitae magna. Sed viverra ullamcorper nulla eu efficitur.",
+        "cover_image": "cars_images/Fiat-Argo-Precision-2019-1.jpg",
+        "gallery_images": [
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/Fiat-Argo-Precision-2019-1.jpg"
+            },
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/Fiat-Argo-Precision-2019-2.jpg"
+            }
+        ],
+        "is_active": true,
+        "created_at": "2023-04-11T14:26:36.000Z",
+        "updated_at": "2023-04-11T14:26:36.000Z",
+        "owner": {
+            "id": "ff46341a-62b2-47c8-8d1f-fc87d7ec5g44",
+            "name": "Gabriela Marchiori"
+        }
+      },
+      {
+        "id": "aaee41d4-d4f4-4b75-8b4e-60e57c47a753",
+        "model": "500e ICON (Elétrico)",
+        "brand": "Fiat",
+        "year": 2022,
+        "fuel": 3,
+        "km": 0,
+        "color": "Cinza",
+        "fip_price": 255821,
+        "price": 277000,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel urna et nisi luctus auctor eget vitae magna. Sed viverra ullamcorper nulla eu efficitur.",
+        "cover_image": "cars_images/fiat-500-eletrico-Icon_1.jpg",
+        "gallery_images": [
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/fiat-500-eletrico-Icon_2.jpg"
+            },
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/fiat-500-eletrico-Icon_1.jpg"
+            }
+        ],
+        "is_active": true,
+        "created_at": "2023-04-11T14:26:36.000Z",
+        "updated_at": "2023-04-11T14:26:36.000Z",
+        "owner": {
+            "id": "ff46341a-62b2-47c8-8d1f-fc87d7ec5g44",
+            "name": "Gabriela Marchiori"
+        }
+      },
+      {
+        "id": "0c9a4345-5f5c-4284-8c2b-4f4b4d7c131f",
+        "model": "ARGO DRIVE S-DESIGN 1.3 8V Flex",
+        "brand": "Fiat",
+        "year": 2022,
+        "fuel": 1,
+        "km": 0,
+        "color": "Preto",
+        "fip_price": 85550,
+        "price": 92400,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel urna et nisi luctus auctor eget vitae magna. Sed viverra ullamcorper nulla eu efficitur.",
+        "cover_image": "cars_images/Fiat_Argo_S-Design_1.avif",
+        "gallery_images": [
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/Fiat_Argo_S-Design_1.avif"
+            },
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/Fiat_Argo_S-Design_2.webp"
+            }
+        ],
+        "is_active": true,
+        "created_at": "2023-04-11T14:26:36.000Z",
+        "updated_at": "2023-04-11T14:26:36.000Z",
+        "owner": {
+            "id": "ff46341a-62b2-47c8-8d1f-fc87d7ec5g44",
+            "name": "Gabriela Marchiori"
+        }
+      },
+
+
+      {
+        "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58292",
+        "model": "C4 CACTUS Rip Curl 1.6 16V Flex Aut.",
+        "brand": "Citroën",
+        "year": 2022,
+        "fuel": 1,
+        "km": 50000,
+        "color": "Cinza",
+        "fip_price": 112077,
+        "price": 112000,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel urna et nisi luctus auctor eget vitae magna. Sed viverra ullamcorper nulla eu efficitur.",
+        "cover_image": "cars_images/CITROEN_C4_CACTUS_1.6.webp",
+        "gallery_images": [
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/Citroen-C4-Cactus-Rip-Curl-2.jpg"
+            },
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/CITROEN_C4_CACTUS_1.6.webp"
+            }
+        ],
+        "is_active": true,
+        "created_at": "2023-04-11T07:56:23.430Z",
+        "updated_at": "2023-04-11T07:56:23.430Z",
+        "owner": {
+            "id": "ff46341a-62b2-47c8-8d1f-fc87d7gf7t23",
+            "name": "Lucas Kauan"
+        }
+      },
+      {
+        "id": "83c0316f-f66a-4138-8163-e6f891d90db2",
+        "model": "C3 Attraction 1.6 Flex 16V 5p Aut.",
+        "brand": "Citroën",
+        "year": 2022,
+        "fuel": 1,
+        "km": 13000,
+        "color": "Verde",
+        "fip_price": 66957,
+        "price": 66000,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel urna et nisi luctus auctor eget vitae magna. Sed viverra ullamcorper nulla eu efficitur.",
+        "cover_image": "cars_images/C3_Attraction_2022_1.webp",
+        "gallery_images": [
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/C3_Attraction_2022_1.webp"
+            },
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/C3_Attraction_2022_2.png"
+            }
+        ],
+        "is_active": true,
+        "created_at": "2023-04-11T07:56:23.430Z",
+        "updated_at": "2023-04-11T07:56:23.430Z",
+        "owner": {
+            "id": "ff46341a-62b2-47c8-8d1f-fc87d7gf7t23",
+            "name": "Lucas Kauan"
+        }
+      },
+      {
+        "id": "ab70c8db-0b15-42a1-b47d-cf85a2e2e1f7",
+        "model": "C4 LOUNGE Live 1.6 Turbo Flex Aut.",
+        "brand": "Citroën",
+        "year": 2022,
+        "fuel": 1,
+        "km": 10000,
+        "color": "Branco",
+        "fip_price": 73549,
+        "price": 75000,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel urna et nisi luctus auctor eget vitae magna. Sed viverra ullamcorper nulla eu efficitur.",
+        "cover_image": "cars_images/C4_LOUNGE_Live_1.6_1.webp",
+        "gallery_images": [
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/C4_LOUNGE_Live_1.6_2.jpg"
+            },
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/C4_LOUNGE_Live_1.6_1.webp"
+            }
+        ],
+        "is_active": true,
+        "created_at": "2023-04-11T07:56:23.430Z",
+        "updated_at": "2023-04-11T07:56:23.430Z",
+        "owner": {
+            "id": "ff46341a-62b2-47c8-8d1f-fc87d7gf7t23",
+            "name": "Lucas Kauan"
+        }
+      },
+      {
+        "id": "dcc0b858-ba6f-4f90-a8e7-4dd0f0cb7c61",
+        "model": "Strada Adventure1.8/ 1.8 LOCKER Flex CD",
+        "brand": "Fiat",
+        "year": 2020,
+        "fuel": 1,
+        "km": 30000,
+        "color": "Preto",
+        "fip_price": 91600,
+        "price": 92000,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel urna et nisi luctus auctor eget vitae magna. Sed viverra ullamcorper nulla eu efficitur.",
+        "cover_image": "cars_images/fiat_strada_2020_1.jpg",
+        "gallery_images": [
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/fiat_strada_2020_1.jpg"
+            },
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/fiat_strada_2020_2.jpg"
+            }
+        ],
+        "is_active": true,
+        "created_at": "2023-04-11T10:20:30.000Z",
+        "updated_at": "2023-04-11T10:20:30.000Z",
+        "owner": {
+            "id": "ff46341a-8d1f-47c8-6l1f-fc87d7e6341a",
+            "name": "Matheus Serruya"
+        }
+      },
+      {
+        "id": "9acfffc9-31bb-4e99-b74a-81f7c31e06d2",
+        "model": "ARGO HGT 1.8 16V Flex Mec.",
+        "brand": "Fiat",
+        "year": 2019,
+        "fuel": 1,
+        "km": 50000,
+        "color": "Azul",
+        "fip_price": 72140,
+        "price": 68000,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel urna et nisi luctus auctor eget vitae magna. Sed viverra ullamcorper nulla eu efficitur.",
+        "cover_image": "cars_images/argo_hgt_2019_1.jpg",
+        "gallery_images": [
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/argo_hgt_2019_2.jpg"
+            },
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/argo_hgt_2019_1.jpg"
+            }
+        ],
+        "is_active": true,
+        "created_at": "2023-04-11T10:20:30.000Z",
+        "updated_at": "2023-04-11T10:20:30.000Z",
+        "owner": {
+            "id": "ff46341a-8d1f-47c8-6l1f-fc87d7e6341a",
+            "name": "Matheus Serruya"
+        }
+      },
+      {
+        "id": "f79c92a6-414e-4d85-9c68-3f9b2a77e14e",
+        "model": "ARGO PRECISION 1.8 16V Flex Mec.",
+        "brand": "Fiat",
+        "year": 2019,
+        "fuel": 1,
+        "km": 80000,
+        "color": "Branco",
+        "fip_price": 69036,
+        "price": 68000,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel urna et nisi luctus auctor eget vitae magna. Sed viverra ullamcorper nulla eu efficitur.",
+        "cover_image": "cars_images/Fiat-Argo-Precision-2019-1.jpg",
+        "gallery_images": [
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/Fiat-Argo-Precision-2019-1.jpg"
+            },
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/Fiat-Argo-Precision-2019-2.jpg"
+            }
+        ],
+        "is_active": true,
+        "created_at": "2023-04-11T14:26:36.000Z",
+        "updated_at": "2023-04-11T14:26:36.000Z",
+        "owner": {
+            "id": "ff46341a-8d1f-47c8-6l1f-fc87d7e6341a",
+            "name": "Matheus Serruya"
+        }
+      },
+      {
+        "id": "aaee41d4-d4f4-4b75-8b4e-60e57c47a753",
+        "model": "500e ICON (Elétrico)",
+        "brand": "Fiat",
+        "year": 2022,
+        "fuel": 3,
+        "km": 20000,
+        "color": "Cinza",
+        "fip_price": 255821,
+        "price": 252000,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel urna et nisi luctus auctor eget vitae magna. Sed viverra ullamcorper nulla eu efficitur.",
+        "cover_image": "cars_images/fiat-500-eletrico-Icon_1.jpg",
+        "gallery_images": [
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/fiat-500-eletrico-Icon_2.jpg"
+            },
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/fiat-500-eletrico-Icon_1.jpg"
+            }
+        ],
+        "is_active": true,
+        "created_at": "2023-04-11T14:26:36.000Z",
+        "updated_at": "2023-04-11T14:26:36.000Z",
+        "owner": {
+            "id": "ff46341a-8d1f-47c8-6l1f-fc87d7e6341a",
+            "name": "Matheus Serruya"
+        }
+      },
+      {
+        "id": "0c9a4345-5f5c-4284-8c2b-4f4b4d7c131f",
+        "model": "ARGO DRIVE S-DESIGN 1.3 8V Flex",
+        "brand": "Fiat",
+        "year": 2022,
+        "fuel": 1,
+        "km": 10000,
+        "color": "Preto",
+        "fip_price": 85550,
+        "price": 90000,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel urna et nisi luctus auctor eget vitae magna. Sed viverra ullamcorper nulla eu efficitur.",
+        "cover_image": "cars_images/Fiat_Argo_S-Design_1.avif",
+        "gallery_images": [
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/Fiat_Argo_S-Design_1.avif"
+            },
+            {
+                "id": "3aeed3e3-719f-4b3a-87c3-3d962bf58211",
+                "url": "cars_images/Fiat_Argo_S-Design_2.webp"
+            }
+        ],
+        "is_active": true,
+        "created_at": "2023-04-11T14:26:36.000Z",
+        "updated_at": "2023-04-11T14:26:36.000Z",
+        "owner": {
+            "id": "ff46341a-8d1f-47c8-6l1f-fc87d7e6341a",
+            "name": "Matheus Serruya"
+        }
+      }
+]
