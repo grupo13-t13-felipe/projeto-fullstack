2c0645a51f4e5f48d0707d8e24d2018c2e32fea3
Lucas Magalhães <lucoder618@gmail.com>
2023-04-25 15:14:39 -0300

    feat: new annoucements route: getAllAnnoucementFilterTypes

--- client/src/components/homeFilter.tsx
+++ client/src/components/homeFilter.tsx
@@ -3,133 +3,201 @@ import { InputFilter } from "./input";
 import { annoucementCtx } from "@/contexts/announcements.context";
 import { useState } from "react";
 
-
 const HomeFilter = () => {
+	const { allFilterAnnouncements, setAllFilterAnnouncements } =
+		annoucementCtx();
+	const arrayFuel: Array<[]> = [];
+	allFilterAnnouncements.map((item: any) => {
+		arrayFuel.push(item.fuel);
+	});
+	const newArrayFuel = arrayFuel.filter(
+		(el, i) => arrayFuel.indexOf(el) === i
+	);
 
-    const {allAnnouncements, setAnnouncement} = annoucementCtx()
-    const arrayFuel: Array<[]> = []
-    allAnnouncements.map((item: any) => {
-        arrayFuel.push(item.fuel)
-    })
-    const newArrayFuel = arrayFuel.filter((el, i) => arrayFuel.indexOf(el) === i)
-    
-    const arrayBrand: Array<[]> = []
-    allAnnouncements.map((item: any) => {
-        arrayBrand.push(item.brand)
-    })
-    const newArrayBrand = arrayBrand.filter((el, i) => arrayBrand.indexOf(el) === i)
+	const arrayBrand: Array<[]> = [];
+	allFilterAnnouncements.map((item: any) => {
+		arrayBrand.push(item.brand);
+	});
+	const newArrayBrand = arrayBrand.filter(
+		(el, i) => arrayBrand.indexOf(el) === i
+	);
 
-    const arrayModel: Array<[]> = []
-    allAnnouncements.map((item: any) => {
-        arrayModel.push(item.model)
-    })
-    const newArrayModel = arrayModel.filter((el, i) => arrayModel.indexOf(el) === i)
+	const arrayModel: Array<[]> = [];
+	allFilterAnnouncements.map((item: any) => {
+		arrayModel.push(item.model);
+	});
+	const newArrayModel = arrayModel.filter(
+		(el, i) => arrayModel.indexOf(el) === i
+	);
 
-    const arrayColor: Array<[]> = []
-    allAnnouncements.map((item: any) => {
-        arrayColor.push(item.color)
-    })
-    const newArrayColor = arrayColor.filter((el, i) => arrayColor.indexOf(el) === i)
+	const arrayColor: Array<[]> = [];
+	allFilterAnnouncements.map((item: any) => {
+		arrayColor.push(item.color);
+	});
+	const newArrayColor = arrayColor.filter(
+		(el, i) => arrayColor.indexOf(el) === i
+	);
 
-    const arrayYear: Array<[]> = []
-    allAnnouncements.map((item: any) => {
-        arrayYear.push(item.year)
-    })
-    const newArrayYear = arrayYear.filter((el, i) => arrayYear.indexOf(el) === i)
+	const arrayYear: Array<[]> = [];
+	allFilterAnnouncements.map((item: any) => {
+		arrayYear.push(item.year);
+	});
+	const newArrayYear = arrayYear.filter(
+		(el, i) => arrayYear.indexOf(el) === i
+	);
 
-    const [getBrand, setGetBrand] = useState()
-    const [getModel, setGetModel] = useState()
-    const [getColor, setGetColor] = useState()
-    const [getYear, setGetYear] = useState()
-    const [getFuel, setGetFuel] = useState()
+	const [getBrand, setGetBrand] = useState();
+	const [getModel, setGetModel] = useState();
+	const [getColor, setGetColor] = useState();
+	const [getYear, setGetYear] = useState();
+	const [getFuel, setGetFuel] = useState();
 
-   
-    const objectFilter = {brand: getBrand, model: getModel, color: getColor, year: getYear, fuel: getFuel}
+	const objectFilter = {
+		brand: getBrand,
+		model: getModel,
+		color: getColor,
+		year: getYear,
+		fuel: getFuel,
+	};
 
-    const sumFilter = () => {
+	const sumFilter = () => {};
 
-    }
+	console.log(objectFilter);
 
-    console.log(objectFilter)
+	return (
+		<Flex direction={"column"}>
+			<List>
+				<Text
+					color={"grey.500"}
+					fontSize={["xl", "2xl", "2xl", "3xl"]}
+					fontWeight={"semibold"}
+					mb={"10px"}
+					mt={"10px"}>
+					Marca
+				</Text>
+				{newArrayBrand.map((item: any, index: any) => {
+					return (
+						<ListItem
+							onClick={() => setGetBrand(item)}
+							color={"grey.250"}
+							fontSize={["md", "lg", "lg", "xl"]}
+							fontWeight={"normal"}>
+							{item[0].toUpperCase() + item.substring(1)}
+						</ListItem>
+					);
+				})}
 
+				<Text
+					color={"grey.500"}
+					fontSize={["xl", "2xl", "2xl", "3xl"]}
+					fontWeight={"semibold"}
+					mb={"10px"}
+					mt={"10px"}>
+					Modelo
+				</Text>
+				{newArrayModel.map((item: any, index: any) => {
+					return (
+						<ListItem
+							onClick={() => setGetModel(item)}
+							color={"grey.250"}
+							fontSize={["md", "lg", "lg", "xl"]}
+							fontWeight={"normal"}>
+							{item[0].toUpperCase() + item.substring(1)}
+						</ListItem>
+					);
+				})}
 
-    
-       
-              
+				<Text
+					color={"grey.500"}
+					fontSize={["xl", "2xl", "2xl", "3xl"]}
+					fontWeight={"semibold"}
+					mb={"10px"}
+					mt={"10px"}>
+					Cor
+				</Text>
+				{newArrayColor.map((item: any, index: any) => {
+					return (
+						<ListItem
+							onClick={() => setGetColor(item)}
+							color={"grey.250"}
+							fontSize={["md", "lg", "lg", "xl"]}
+							fontWeight={"normal"}>
+							{item[0].toUpperCase() + item.substring(1)}
+						</ListItem>
+					);
+				})}
 
-    return (
-        <Flex direction={'column'}>
-          <List>
+				<Text
+					color={"grey.500"}
+					fontSize={["xl", "2xl", "2xl", "3xl"]}
+					fontWeight={"semibold"}
+					mb={"10px"}
+					mt={"10px"}>
+					Ano
+				</Text>
+				{newArrayYear.map((item: any, index: any) => {
+					return (
+						<ListItem
+							onClick={() => setGetYear(item)}
+							color={"grey.250"}
+							fontSize={["md", "lg", "lg", "xl"]}
+							fontWeight={"normal"}>
+							{item}
+						</ListItem>
+					);
+				})}
 
-           <Button onClick={() => sumFilter()}>Soma</Button>
-          <Text color={'grey.500'} fontSize={['xl', '2xl','2xl', '3xl']} fontWeight={'semibold'} mb={'10px'} mt={'10px'}>Marca</Text>
-            {
-                newArrayBrand.map((item: any, index: any) => {
-                   return (
-                        <ListItem onClick={() => setGetBrand(item)} color={'grey.250'} fontSize={['md', 'lg','lg','xl']} fontWeight={'normal'}>{item[0].toUpperCase() + item.substring(1)}</ListItem>
-                   )
-                })
-            }
+				<Text
+					color={"grey.500"}
+					fontSize={["xl", "2xl", "2xl", "3xl"]}
+					fontWeight={"semibold"}
+					mb={"10px"}
+					mt={"10px"}>
+					Combustível
+				</Text>
+				{newArrayFuel.map((item: any, index: any) => {
+					return (
+						<ListItem
+							onClick={() => setGetFuel(item)}
+							color={"grey.250"}
+							fontSize={["md", "lg", "lg", "xl"]}
+							fontWeight={"normal"}>
+							{item[0].toUpperCase() + item.substring(1)}
+						</ListItem>
+					);
+				})}
+			</List>
 
-        <Text color={'grey.500'} fontSize={['xl', '2xl','2xl', '3xl']} fontWeight={'semibold'} mb={'10px'} mt={'10px'}>Modelo</Text>
-            {
-                newArrayModel.map((item: any, index: any) => {
-                   return (
-                        <ListItem onClick={() => setGetModel(item)} color={'grey.250'} fontSize={['md', 'lg','lg','xl']} fontWeight={'normal'}>{item[0].toUpperCase() + item.substring(1)}</ListItem>
-                   )
-                })
-            }
+			<List>
+				<Text
+					color={"grey.500"}
+					fontSize={["xl", "2xl", "2xl", "3xl"]}
+					fontWeight={"semibold"}
+					mb={"10px"}
+					mt={"10px"}>
+					Km
+				</Text>
+				<Flex direction={"row"} gap={"12px"}>
+					<InputFilter placeholder={"Mínimo"} />
+					<InputFilter placeholder={"Máximo"} />
+				</Flex>
+			</List>
+			<List>
+				<Text
+					color={"grey.500"}
+					fontSize={["xl", "2xl", "2xl", "3xl"]}
+					fontWeight={"semibold"}
+					mb={"10px"}
+					mt={"10px"}>
+					Preço
+				</Text>
+				<Flex direction={"row"} gap={"12px"}>
+					<InputFilter placeholder={"Mínimo"} />
+					<InputFilter placeholder={"Máximo"} />
+				</Flex>
+			</List>
+		</Flex>
+	);
+};
 
-        <Text color={'grey.500'} fontSize={['xl', '2xl','2xl', '3xl']} fontWeight={'semibold'} mb={'10px'} mt={'10px'}>Cor</Text>
-            {
-                newArrayColor.map((item: any, index: any) => {
-                   return (
-                        <ListItem  onClick={() => setGetColor(item)} color={'grey.250'} fontSize={['md', 'lg','lg','xl']} fontWeight={'normal'}>{item[0].toUpperCase() + item.substring(1)}</ListItem>
-                   )
-                })
-            }
-
-        <Text color={'grey.500'} fontSize={['xl', '2xl','2xl', '3xl']} fontWeight={'semibold'} mb={'10px'} mt={'10px'}>Ano</Text>
-            {
-                newArrayYear.map((item: any, index: any) => {
-                   return (
-                        <ListItem onClick={() => setGetYear(item)} color={'grey.250'} fontSize={['md', 'lg','lg','xl']} fontWeight={'normal'}>{item}</ListItem>
-                   )
-                })
-            }
-            
-        <Text color={'grey.500'} fontSize={['xl', '2xl','2xl', '3xl']} fontWeight={'semibold'} mb={'10px'} mt={'10px'}>Combustível</Text>
-            {
-                newArrayFuel.map((item: any, index: any) => {
-                   return (
-                        <ListItem onClick={() => setGetFuel(item)} color={'grey.250'} fontSize={['md', 'lg','lg','xl']} fontWeight={'normal'}>{item[0].toUpperCase() + item.substring(1)}</ListItem>
-                   )
-                })
-            }
-           
-          </List>
-
-          <List>
-            <Text color={'grey.500'} fontSize={['xl', '2xl','2xl', '3xl']} fontWeight={'semibold'} mb={'10px'} mt={'10px'}>Km</Text>
-            <Flex direction={'row'} gap={"12px"}>
-                <InputFilter placeholder={"Mínimo"}/>
-                <InputFilter placeholder={"Máximo"}/>
-
-            </Flex>
-          </List>
-          <List>
-            <Text color={'grey.500'} fontSize={['xl', '2xl','2xl', '3xl']} fontWeight={'semibold'} mb={'10px'} mt={'10px'}>Preço</Text>
-            <Flex direction={'row'} gap={"12px"}>
-                <InputFilter placeholder={"Mínimo"}/>
-                <InputFilter placeholder={"Máximo"}/>
-
-            </Flex>
-          </List>
-            
-            
-            
-        </Flex>
-    )
-}
-
-export default HomeFilter
+export default HomeFilter;
--- client/src/contexts/announcements.context.tsx
+++ client/src/contexts/announcements.context.tsx
@@ -1,84 +1,109 @@
-import React, { ReactNode, createContext, useContext, useEffect, useState } from "react"
-import { useRouter } from "next/router"
-import api from "@/services/api"
+import React, {
+	ReactNode,
+	createContext,
+	useContext,
+	useEffect,
+	useState,
+} from "react";
+import { useRouter } from "next/router";
+import api from "@/services/api";
 import { IAnnouncement, IAnnouncementOwner } from "@/types/announcements";
 import { UserContext } from "./users.context";
 
-
 interface AnnouncementProviderData {
-    allAnnouncements: IAnnouncement[] | any;
-    setAnnouncement: React.Dispatch<React.SetStateAction<IAnnouncement[] | any>>
-    loading: boolean;
-    setLoading: React.Dispatch<React.SetStateAction<boolean>>
-    announcementsByOwner: IAnnouncement[] | undefined
-    getOwnerById: (ownerId: string | undefined) => Promise<void>
-    ownerId: string | undefined
-    setOwnerId: React.Dispatch<React.SetStateAction<string | undefined>>
-    owner: IAnnouncementOwner | undefined
+	allAnnouncements: IAnnouncement[] | any;
+	setAllAnnouncements: React.Dispatch<
+		React.SetStateAction<IAnnouncement[] | any>
+	>;
+	allFilterAnnouncements: IAnnouncement[];
+	setAllFilterAnnouncements: React.Dispatch<
+		React.SetStateAction<IAnnouncement[] | any>
+	>;
+	loading: boolean;
+	setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+	announcementsByOwner: IAnnouncement[] | undefined;
+	getOwnerById: (ownerId: string | undefined) => Promise<void>;
+	ownerId: string | undefined;
+	setOwnerId: React.Dispatch<React.SetStateAction<string | undefined>>;
+	owner: IAnnouncementOwner | undefined;
 }
 
 export interface IProviderProps {
-    children: ReactNode
+	children: ReactNode;
 }
 
-const AnnouncementContext = createContext<AnnouncementProviderData>({} as AnnouncementProviderData)
+const AnnouncementContext = createContext<AnnouncementProviderData>(
+	{} as AnnouncementProviderData
+);
 
 export const AnnouncementProvider = ({ children }: IProviderProps) => {
-    const router = useRouter()
-    const [allAnnouncements, setAnnouncement] = useState()
-    const [loading, setLoading] = useState(true)
-    const [announcementsByOwner, setAnnouncementsByOwner] = useState<IAnnouncement[] | undefined>()
-    const [ownerId, setOwnerId] = useState<string>()
-    const [owner, setOwner] = useState<IAnnouncementOwner>()
-    const { user } = useContext(UserContext)
+	const router = useRouter();
+	const [allAnnouncements, setAllAnnouncements] = useState();
+	const [allFilterAnnouncements, setAllFilterAnnouncements] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [announcementsByOwner, setAnnouncementsByOwner] = useState<
+		IAnnouncement[] | undefined
+	>();
+	const [ownerId, setOwnerId] = useState<string>();
+	const [owner, setOwner] = useState<IAnnouncementOwner>();
+	const { user } = useContext(UserContext);
 
-    async function getAllAnnouncements() {
+	async function getAllAnnouncements() {
+		try {
+			const { data } = await api.get("/annoucements");
+			setAllAnnouncements(data.data);
+			setLoading(false);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-        try {
-            const { data } = await api.get("/annoucements")
-            setAnnouncement(data.data)
-            setLoading(false)
-        }
-        catch (error) {
-            console.log(error)
-        }
-    }
+	async function getAllAnnouncementsByIdOwner(ownerId: string) {
+		try {
+			const { data } = await api.get(`/annoucements?owner_id=${ownerId}`);
+			setAnnouncementsByOwner(data.data);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setTimeout(() => {
+				setLoading(false);
+				router.push("/announcements");
+			}, 1000);
+		}
+	}
 
-    async function getAllAnnouncementsByIdOwner(ownerId: string) {
-        try {
-            const { data } = await api.get(`/annoucements?owner_id=${ownerId}`)
-            setAnnouncementsByOwner(data.data)
-        } catch (error) {
-            console.error(error)
-        } finally {
-            setTimeout(() => {
-                setLoading(false)
-                router.push("/announcements")
-            }, 1000);
-        }
-    }
+	async function getOwnerById(ownerId: string | undefined) {
+		try {
+			const { data } = await api.get(`/users/${ownerId}`);
+			setOwner(data);
+			getAllAnnouncementsByIdOwner(ownerId!);
+		} catch (error) {
+			console.error(error);
+		}
+	}
 
-    async function getOwnerById(ownerId: string | undefined) {
-        try {
-            const { data } = await api.get(`/users/${ownerId}`)
-            setOwner(data)
-            getAllAnnouncementsByIdOwner(ownerId!)
+	useEffect(() => {
+		getAllAnnouncements();
+	}, []);
 
-        } catch (error) {
-            console.error(error)
-        }
-    }
+	return (
+		<AnnouncementContext.Provider
+			value={{
+				allAnnouncements,
+				setAllAnnouncements,
+				allFilterAnnouncements,
+				setAllFilterAnnouncements,
+				loading,
+				setLoading,
+				announcementsByOwner,
+				getOwnerById,
+				ownerId,
+				setOwnerId,
+				owner,
+			}}>
+			{children}
+		</AnnouncementContext.Provider>
+	);
+};
 
-    useEffect(() => {
-        getAllAnnouncements()
-    }, [])
-
-
-    return (
-        <AnnouncementContext.Provider value={{ allAnnouncements, setAnnouncement, loading, setLoading, announcementsByOwner, getOwnerById, ownerId, setOwnerId, owner }}>
-            {children}
-        </AnnouncementContext.Provider>
-    )
-}
-
-export const annoucementCtx = () => useContext(AnnouncementContext)
+export const annoucementCtx = () => useContext(AnnouncementContext);
