f9714db6f1d3d0ee6013cb0a35fd574e7d69b810
Lucas Magalhães <lucoder618@gmail.com>
2023-04-27 17:11:52 -0300

    fix: button onclick function

--- client/src/components/button.tsx
+++ client/src/components/button.tsx
@@ -1,13 +1,36 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
-const Buttons = ({ onClick, backgroundColor, color, fontSize, valueButton, hover, border, radius, borderColor, width, maxWidth, margin}: any) => {
-    return (
-        <Button onClick={onClick} background={backgroundColor} color={color} variant='solid' fontSize={fontSize} _hover={{ bg: hover }} border={border} borderRadius={radius} borderColor={borderColor} width={width} margin={margin} maxWidth={maxWidth}>
-            {
-                valueButton
-            }
-        </Button>
-    )
-}
+const Buttons = ({
+	onClick,
+	backgroundColor,
+	color,
+	fontSize,
+	valueButton,
+	hover,
+	border,
+	radius,
+	borderColor,
+	width,
+	maxWidth,
+	margin,
+}: any) => {
+	return (
+		<Button
+			onClick={onClick}
+			background={backgroundColor}
+			color={color}
+			variant="solid"
+			fontSize={fontSize}
+			_hover={{ bg: hover }}
+			border={border}
+			borderRadius={radius}
+			borderColor={borderColor}
+			width={width}
+			margin={margin}
+			maxWidth={maxWidth}>
+			{valueButton}
+		</Button>
+	);
+};
 
 export default Buttons;
--- client/src/pages/send-email.tsx
+++ client/src/pages/send-email.tsx
@@ -1,37 +1,76 @@
-import DefaultHeader from '@/components/headers/headerDefault';
-import DefaultFooter from '@/components/footer';
-import DefaultForm from '@/components/form';
-import { Button, Flex, Text, Link } from '@chakra-ui/react';
-import { InputForm } from '@/components/input';
-import { useContext, useState } from 'react';
-import { UserContext } from '@/contexts/users.context';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from "@hookform/resolvers/yup"
-import { IUserLogin } from '@/types/user';
-import { userLoginSchema } from '@/schemas/user.schema';
-import Buttons from '@/components/button';
+import DefaultHeader from "@/components/headers/headerDefault";
+import DefaultFooter from "@/components/footer";
+import DefaultForm from "@/components/form";
+import { Button, Flex, Text, Link } from "@chakra-ui/react";
+import { InputForm } from "@/components/input";
+import { useContext, useState } from "react";
+import { UserContext } from "@/contexts/users.context";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { IUserLogin } from "@/types/user";
+import { userLoginSchema } from "@/schemas/user.schema";
+import Buttons from "@/components/button";
 
 const SendEmail = () => {
+	const { loginUser } = useContext(UserContext);
 
-    const { loginUser } = useContext(UserContext)
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<IUserLogin>({
+		resolver: yupResolver(userLoginSchema),
+	});
 
+	return (
+		<>
+			<DefaultHeader />
+			<Flex
+				minH={"600px"}
+				h={[
+					"calc(100vh - 60px - 149px)",
+					"calc(100vh - 60px - 149px)",
+					"calc(100vh - 60px - 120px)",
+				]}
+				justifyContent={"center"}
+				alignItems={"center"}
+				bgColor={"grey.50"}
+				p={"16px"}>
+				<DefaultForm
+					submithandler={handleSubmit(loginUser)}
+					formtitle="Inserir e-mail para recuperação">
+					<InputForm
+						errors={errors.email?.message}
+						isInvalid={!!errors.email?.message}
+						inputregister={{ ...register("email") }}
+						isRequired
+						mb={"12px"}
+						labeltext={"Email"}
+						inputplaceholder={"Digitar email"}
+						inputtype={"text"}
+					/>
+					<Buttons
+						onClick={() => {
+							//lógica vai aqui:
+							console.log("oi");
+						}}
+						backgroundColor={"blue.300"}
+						color={"white"}
+						fontSize={"md"}
+						valueButton={"Enviar e-mail"}
+						hover={"black"}
+						border={"solid 2px"}
+						radius={"5px"}
+						borderColor={"black"}
+						width={"100%"}
+						maxWidth={"200px"}
+						margin={"2px"}
+					/>
+				</DefaultForm>
+			</Flex>
+			<DefaultFooter />
+		</>
+	);
+};
 
-    const { register, handleSubmit, formState: { errors } } = useForm<IUserLogin>({
-        resolver: yupResolver(userLoginSchema)
-    })
-
-    return (
-        <>
-            <DefaultHeader />
-            <Flex minH={"600px"} h={["calc(100vh - 60px - 149px)", "calc(100vh - 60px - 149px)", "calc(100vh - 60px - 120px)"]} justifyContent={"center"} alignItems={"center"} bgColor={"grey.50"} p={"16px"}>
-                <DefaultForm submithandler={handleSubmit(loginUser)} formtitle="Inserir e-mail para recuperação">
-                    <InputForm errors={errors.email?.message} isInvalid={!!errors.email?.message} inputregister={{ ...register("email") }} isRequired mb={"12px"} labeltext={"Email"} inputplaceholder={"Digitar email"} inputtype={"text"} />
-                    <Buttons onClick={console.log("oi")}  backgroundColor={"blue.300"} color={"white"} fontSize={"md"} valueButton={"Enviar e-mail"} hover={"black"} border={"solid 2px"} radius={"5px"} borderColor={"black"} width={"100%"} maxWidth={"200px"} margin={"2px"}/>
-                </DefaultForm>
-            </Flex>
-            <DefaultFooter />
-        </>
-    )
-}
-
-export default SendEmail
+export default SendEmail;
