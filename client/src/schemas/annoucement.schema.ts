710ad20da4c37657079a0b8f965085624ab31a62
Matheus Serruya <matheus.serruya07@gmail.com>
2023-04-27 12:52:11 -0300

    Set functional annoucement creation modal

---
+++ client/src/schemas/annoucement.schema.ts
@@ -0,0 +1,17 @@
+import * as yup from "yup";
+
+export const createAnnouncementSchema = yup.object().shape({
+    model: yup.string().required("O modelo é obrigatório"),
+    brand: yup.string().required("A marca é obrigatória"),
+    year: yup.string().required("O ano é obrigatório"),
+    fuel: yup.string().required("O combustivel é obrigatório"),
+    km: yup.string().required("A quilometragem é obrigatória"),
+    color: yup.string().required("A cor é obrigatória"),
+    fip_price: yup.string().required("O preço FIP é obrigatório"),
+    price: yup.string().required("O preço é obrigatório"),
+    description: yup.string().required("A descrição é obrigatória"),
+    cover_image: yup.string().required("A imagem de capa é obrigatória"),
+    // gallery_images: yup.array().of(
+    //     yup.string().notRequired()
+    // ).notRequired()
+});
