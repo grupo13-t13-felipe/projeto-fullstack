7e383b81905d40479c753a23bbc16daaeec5f5c5
Kauan <kauancompositor@outlook.com>
2023-04-27 17:00:34 -0300

    Add partial front-send-email

--- client/src/pages/send-email.tsx
+++ client/src/pages/send-email.tsx
@@ -3,16 +3,18 @@ import DefaultFooter from '@/components/footer';
 import DefaultForm from '@/components/form';
 import { Button, Flex, Text, Link } from '@chakra-ui/react';
 import { InputForm } from '@/components/input';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { UserContext } from '@/contexts/users.context';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from "@hookform/resolvers/yup"
 import { IUserLogin } from '@/types/user';
 import { userLoginSchema } from '@/schemas/user.schema';
+import Buttons from '@/components/button';
 
-const Login = () => {
+const SendEmail = () => {
 
     const { loginUser } = useContext(UserContext)
+
 
     const { register, handleSubmit, formState: { errors } } = useForm<IUserLogin>({
         resolver: yupResolver(userLoginSchema)
@@ -22,13 +24,9 @@ const Login = () => {
         <>
             <DefaultHeader />
             <Flex minH={"600px"} h={["calc(100vh - 60px - 149px)", "calc(100vh - 60px - 149px)", "calc(100vh - 60px - 120px)"]} justifyContent={"center"} alignItems={"center"} bgColor={"grey.50"} p={"16px"}>
-                <DefaultForm submithandler={handleSubmit(loginUser)} formtitle="Login">
+                <DefaultForm submithandler={handleSubmit(loginUser)} formtitle="Inserir e-mail para recuperação">
                     <InputForm errors={errors.email?.message} isInvalid={!!errors.email?.message} inputregister={{ ...register("email") }} isRequired mb={"12px"} labeltext={"Email"} inputplaceholder={"Digitar email"} inputtype={"text"} />
-                    <InputForm errors={errors.password?.message} isInvalid={!!errors.password?.message} inputregister={{ ...register("password") }} isRequired labeltext={"Senha"} inputplaceholder={"Digitar senha"} inputtype={"password"} />
-                    <Link mb={"12px"} w={"100%"} fontWeight={"400"} textAlign={"right"} href={`/send-email`}>Esqueci minha senha</Link>
-                    <Button mb={"12px"} w={"100%"} h={"48px"} color={"white"} fontWeight={"500"} bgColor={"blue.300"} border={"1px solid blue.300"} _hover={{ borderColor: "blue.400", bgColor: "blue.400" }} type={"submit"}>Entrar</Button>
-                    <Text mb={"12px"} fontWeight={"400"} fontSize={"14px"}>Ainda não possui conta?</Text>
-                    <Link display={"inline-flex"} justifyContent={"center"} alignItems={"center"} mb={"12px"} w={"100%"} h={"48px"} fontWeight={"500"} backgroundColor={"grey.25"} border={"2px solid"} borderColor={"grey.200"} borderRadius={"4px"} _hover={{ textDecoration: "none", borderColor: "grey.200", bgColor: "grey.100" }} href={`/register`}>Cadastrar</Link>
+                    <Buttons onClick={console.log("oi")}  backgroundColor={"blue.300"} color={"white"} fontSize={"md"} valueButton={"Enviar e-mail"} hover={"black"} border={"solid 2px"} radius={"5px"} borderColor={"black"} width={"100%"} maxWidth={"200px"} margin={"2px"}/>
                 </DefaultForm>
             </Flex>
             <DefaultFooter />
@@ -36,4 +34,4 @@ const Login = () => {
     )
 }
 
-export default Login
+export default SendEmail
