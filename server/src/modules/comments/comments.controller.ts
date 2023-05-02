b6566d2e7dd57a852d847b427167535ac0d2e1b3
Lucas Magalhães <lucoder618@gmail.com>
2023-05-02 12:35:35 -0300

    feat: comments / findAllByAnnoucement

--- server/src/modules/comments/comments.controller.ts
+++ server/src/modules/comments/comments.controller.ts
@@ -11,31 +11,31 @@ import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 
-@Controller('comments')
+@Controller()
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
-  @Post()
+  @Post('annoucements/:annoucement_id/comments')
   create(@Body() createCommentDto: CreateCommentDto) {
     return this.commentsService.create(createCommentDto);
   }
 
-  @Get()
-  findAll() {
-    return this.commentsService.findAll();
+  @Get('annoucements/:annoucement_id/comments')
+  findAllByAnnoucement(@Param('annoucement_id') annoucement_id: string) {
+    return this.commentsService.findAllByAnnoucement(annoucement_id);
   }
 
-  @Get(':id')
+  /* @Get(':id')
   findOne(@Param('id') id: string) {
     return this.commentsService.findOne(+id);
-  }
+  } */
 
-  @Patch(':id')
+  @Patch('comments/:comment_id')
   update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
     return this.commentsService.update(+id, updateCommentDto);
   }
 
-  @Delete(':id')
+  @Delete('comments/:comment_id')
   remove(@Param('id') id: string) {
     return this.commentsService.remove(+id);
   }
--- server/src/modules/comments/comments.service.ts
+++ server/src/modules/comments/comments.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class CommentsService {
+  constructor(private prisma: PrismaService) {}
+
   create(createCommentDto: CreateCommentDto) {
     return 'This action adds a new comment';
   }
 
-  findAll() {
-    return [];
+  async findAllByAnnoucement(annoucement_id: string) {
+    const result = await this.prisma.comment.findMany({
+      where: {
+        announcement_id: annoucement_id,
+      },
+    });
+    return result;
   }
 
   findOne(id: number) {
