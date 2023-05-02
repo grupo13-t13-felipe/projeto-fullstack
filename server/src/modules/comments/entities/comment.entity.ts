29f068d7d9ba7abf14e87b3f34f65e7fec40fb29
Lucas Magalhães <lucoder618@gmail.com>
2023-05-02 12:13:13 -0300

    feat: comments dto and entity

--- server/src/modules/comments/entities/comment.entity.ts
+++ server/src/modules/comments/entities/comment.entity.ts
@@ -1,1 +1,8 @@
-export class Comment {}
+export class Comment {
+  readonly id: string;
+  content: string;
+  created_at: Date;
+  updated_at: Date;
+  owner_id: string;
+  annoucement_id: string;
+}
